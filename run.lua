c9254465a181514b5cc567bfa003bf1d1b57d13a
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-13 06:13:04 +0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -1,10 +1,10 @@
 local player = game.Players.LocalPlayer
 local char = player.Character
 local humanoid = char.Humanoid
+local health = humanoid.Health
+local maxhealth = humanoid.MaxHealth
+local healthchangeconnect
+local maxhealthchangeconnect
 local function heal()
-while true do
-    task.wait(0.1)
-    if humanoid.Health < humanoid.MaxHealth then
-        humanoid.Health = humanoid.MaxHealth
-    end
+    health = maxhealth
 end

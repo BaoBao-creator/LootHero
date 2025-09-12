1f242b5b266bd2b439b206e033bbc53d91daad02
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-13 06:15:56 +0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -3,8 +3,12 @@ local char = player.Character
 local humanoid = char.Humanoid
 local health = humanoid.Health
 local maxhealth = humanoid.MaxHealth
-local healthchangeconnect
-local maxhealthchangeconnect
 local function heal()
     health = maxhealth
 end
+humanoid.HealthChanged:Connect(function()
+    heal()
+end)
+humanoid:GetPropertyChangedSignal("MaxHealth"):Connect(function()
+    maxhealth = humanoid.MaxHealth
+end)
